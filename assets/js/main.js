a691e3c05b5b53a8ce3feae7521759335698a992
Soroush Karimi <sorooshkarimi93@gmail.com>
2023-03-10 23:58:27 +0330

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,50 @@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@      SHOW & REMOVE MENU      @@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+const navToggle = document.querySelector('.nav__toggle'),
+  navClose = document.querySelector('.nav__close'),
+  navMenu = document.querySelector('.nav__menu'),
+  navLogo = document.querySelector('.nav__menu-logo'),
+  overlay = document.querySelector('.overlay');
+
+navToggle.addEventListener('click', () => {
+  navMenu.classList.add('show-menu');
+  overlay.classList.add('show-overlay');
+});
+
+navClose.addEventListener('click', () => {
+  navMenu.classList.remove('show-menu');
+  overlay.classList.remove('show-overlay');
+});
+
+navLogo.addEventListener('click', () => {
+  navMenu.classList.remove('show-menu');
+  overlay.classList.remove('show-overlay');
+});
+
+overlay.addEventListener('click', () => {
+  navMenu.classList.remove('show-menu');
+  overlay.classList.remove('show-overlay');
+});
+
+const navLinks = document.querySelectorAll('.nav__link');
+navLinks.forEach((e) => {
+  e.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+    overlay.classList.remove('show-overlay');
+  });
+});
+
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@    SHOW & REMOVE DROPDOWN    @@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+const dropdownButtons = document.querySelectorAll('.dropdown-box');
+dropdownButtons.forEach((e) => {
+  e.addEventListener('click', function () {
+    this.parentElement.classList.toggle('active');
+  });
+});
+
 // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 // @@@@@@@@@      HOME TITLE ANIMATION      @@@@@@@@@
 // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
@@ -19,6 +66,3 @@ function homeAnimation() {
 
 homeAnimation();
 setInterval(homeAnimation, 12000);
-
-
-
